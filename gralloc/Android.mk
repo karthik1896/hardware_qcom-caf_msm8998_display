66b3f3e810373ab8a898c090e102dcef5898c887
RONIN <71311970+RONIN808@users.noreply.github.com>
2022-04-20 10:41:15 +0530

    gralloc: Fix built error

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wall -Werror -Wno-enum-enum-conversion
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libsync libgrallocutils \
                                  android.hardware.graphics.common@1.1
